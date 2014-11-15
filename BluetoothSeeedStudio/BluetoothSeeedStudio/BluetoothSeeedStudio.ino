ba237d55b9ffd82e83a819b5ecc75c353598f733
Daniel Mirmilshteyn <daniel.mirmilshteyn@outlook.com>
2014-11-14 19:28:38 -0500

    Added sample code for the seeed studio bluetooth module and code for the ZX sensor

---
+++ BluetoothSeeedStudio/BluetoothSeeedStudio/BluetoothSeeedStudio.ino
@@ -0,0 +1,44 @@
+#include <SoftwareSerial.h> //Software Serial Port
+#define RxD 6
+#define TxD 7
+#define DEBUG_ENABLED 1
+
+SoftwareSerial blueToothSerial(RxD,TxD);
+
+void setup()
+{
+Serial.begin(9600);
+pinMode(RxD, INPUT);
+pinMode(TxD, OUTPUT);
+setupBlueToothConnection();
+}
+void loop()
+{
+char recvChar;
+while(1)
+{
+if(blueToothSerial.available())
+{//check if there's any data sent from the remote bluetooth shield
+recvChar = blueToothSerial.read();
+Serial.print(recvChar);
+}
+if(Serial.available())
+{//check if there's any data sent from the local serial terminal, you can add the other applications here
+recvChar = Serial.read();
+blueToothSerial.print(recvChar);
+}
+}
+}
+void setupBlueToothConnection()
+{
+blueToothSerial.begin(38400); // Set BluetoothBee BaudRate to default baud rate 38400
+blueToothSerial.print("\r\n+STWMOD=0\r\n"); // set the bluetooth work in slave mode
+blueToothSerial.print("\r\n+STNA=SeeedBTSlave\r\n"); // set the bluetooth name as "SeeedBTSlave"
+blueToothSerial.print("\r\n+STOAUT=1\r\n"); // Permit Paired device to connect me
+blueToothSerial.print("\r\n+STAUTO=0\r\n"); // Auto-connection should be forbidden here
+delay(2000); // This delay is required.
+blueToothSerial.print("\r\n+INQ=1\r\n"); // make the slave bluetooth inquirable
+Serial.println("The slave bluetooth is inquirable!");
+delay(2000); // This delay is required.
+blueToothSerial.flush();
+}
---
+++ ZX_arduino_I2C_demo/ZX_arduino_I2C_demo.ino
@@ -0,0 +1,53 @@
+// Written by Andrew Lohbihler for XYZ Interactive
+// Copyright (c) 2014 XYZ Interactive
+
+#include <Wire.h>
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+}
+
+void loop(void)
+{
+  byte x,z,g;
+  
+  Wire.beginTransmission(0x10);
+  Wire.write(0x08);
+  Wire.endTransmission();
+  Wire.requestFrom(0x10, 1, true);
+  if (Wire.available()) {
+    x = Wire.read();
+  }
+
+  Wire.beginTransmission(0x10);
+  Wire.write(0x0a);
+  Wire.endTransmission();
+  Wire.requestFrom(0x10, 1, true);
+  if (Wire.available()) {
+    z = Wire.read();
+  }
+
+  Wire.beginTransmission(0x10);
+  Wire.write(0x04);
+  Wire.endTransmission();
+  Wire.requestFrom(0x10, 1, true);
+  if (Wire.available()) {
+    g = Wire.read();
+  }
+
+  Serial.print("X = ");
+  Serial.print(x);
+  Serial.print(", Z = ");
+  Serial.print(z);
+  
+  if (g != 0) {
+    Serial.print(" gesture ");
+    Serial.print(g);
+  }
+  
+  Serial.println(".");
+  
+  delay(100);  
+}
